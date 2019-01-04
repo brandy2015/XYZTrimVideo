548cfefd870a36bb645cff0f81cb5ab291e937e6
张子豪 <zhangzihao@mac-mini.local>
2019-01-04 20:33:38 +0800

    Initial Commit

---
+++ XYZTrimVideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XYZTrimVideo
+//
+//  Created by 张子豪 on 2019/1/4.
+//  Copyright © 2019 张子豪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
